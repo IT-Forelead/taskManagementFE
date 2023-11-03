d95b9b4f23a1b714b87d57b23fd9ce7a602f1835
Surojiddin <isurojiddin@gmail.com>
2023-11-03 15:35:06 +0500

    auth bug fixed

--- src/stores/auth.store.js
+++ src/stores/auth.store.js
@@ -1,47 +1,49 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
-import authService from "../services/auth.service";
-import { toast } from "vue-sonner";
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import authService from '../services/auth.service'
+import { toast } from 'vue-sonner'
 
-export const useAuthStore = defineStore("auth", () => {
-  const user = ref(null);
-  const isLoading = ref(false);
-  const isLoggedIn = ref(false);
+export const useAuthStore = defineStore('auth', () => {
+  const user = ref(null)
+  const isLoading = ref(false)
+  const isLoggedIn = ref(false)
 
+
+  user.value = JSON.parse(localStorage.getItem('session'))
 
   function login(login, password) {
     if (!login) {
-      toast.error("Iltimos loginni kiriting!");
+      toast.error("Iltimos loginni kiriting!")
     } else if (!password) {
-      toast.error("Iltimos parolni kiriting!");
+      toast.error("Iltimos parolni kiriting!")
     } else {
-      isLoading.value = true;
+      isLoading.value = true
       authService
         .login({
           login: login,
           password: password,
         })
         .then((res) => {
-          user.value = res?.data;
-          localStorage.setItem("session", JSON.stringify(res?.data));
-          isLoggedIn.value = true;
-          window.location.href = "/dashboard";
+          user.value = res?.data
+          localStorage.setItem('session', JSON.stringify(res?.data))
+          isLoggedIn.value = true
+          window.location.href = "/dashboard"
         })
         .catch((err) => {
-          console.log(err);
-          toast.error("Login yoki parol noto`g`ri!");
+          isLoading.value = false
+          toast.error("Login yoki parol noto`g`ri!")
         })
         .finally(() => {
-          isLoading.value = false;
-        });
+          isLoading.value = false
+        })
     }
   }
 
   function logout() {
-    authService.logout();
-    user.value = null;
-    window.location.reload();
+    authService.logout()
+    user.value = null
+    window.location.reload()
   }
 
-  return { user, login, logout, isLoading, isLoggedIn };
-});
+  return { user, login, logout, isLoading, isLoggedIn }
+})
