5a5ccfe205385a47a41342d4937e4cdffe0f9134
Surojiddin <isurojiddin@gmail.com>
2023-11-05 18:53:11 +0500

    add task modal improved

--- src/components/modals/AddTaskModal.vue
+++ src/components/modals/AddTaskModal.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useModalStore } from '../../stores/modal.store'
+import { useTaskStore } from '../../stores/task.store'
 import SpinnerIcon from '../../assets/icons/SpinnerIcon.vue'
 import XIcon from '../../assets/icons/XIcon.vue'
 import { ref, reactive } from 'vue'
@@ -42,16 +43,24 @@ const createTask = (id) => {
     assetId: id,
     dueDate: taskForm.dueDate,
     description: taskForm.description
+  }).then(() => {
+    toast.success("Successfully created task")
+    isLoading.value = false
+    TaskService.getTasks({})
+      .then((res) => {
+        useTaskStore().clearStore()
+        setTimeout(() => {
+          useTaskStore().setTasks(res)
+        }, 500)
+      })
+      .catch(() => {
+        toast.error("Error while geting tasks!")
+      })
+    closeModal();
+  }).catch((err) => {
+    toast.error("Error creating task")
+    isLoading.value = false
   })
-    .then(() => {
-      toast.success("Successfully created task")
-      isLoading.value = false
-      closeModal();
-    })
-    .catch((err) => {
-      toast.error("Error creating task")
-      isLoading.value = false
-    })
 }
 
 const uploadFile = () => {
@@ -83,7 +92,6 @@ const submitData = () => {
     uploadFile()
   }
 }
-
 </script>
 <template>
   <div v-if="useModalStore().isOpenAddTaskModal"
@@ -121,7 +129,7 @@ const submitData = () => {
             <input v-model="taskForm.dueDate" id="due-date" type="date"
               class="block w-full rounded border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
-          <div>
+          <!-- <div>
             <label for="assign" class="mb-1 text-sm font-medium leading-6 text-gray-900">Ижрочиларни танлаш</label>
             <select id="assign"
               class="block w-full px-2 py-2 text-gray-900 bg-white border-0 rounded shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
@@ -129,20 +137,20 @@ const submitData = () => {
               <option>Canada</option>
               <option>Mexico</option>
             </select>
-          </div>
+          </div> -->
           <div>
             <label for="description" class="mb-1 text-sm font-medium leading-6 text-gray-900">Хужжат мазмуни</label>
             <textarea v-model="taskForm.description" id="description" rows="4" placeholder="Мазмунини киритиш..."
               class="block w-full rounded border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
           </div>
         </div>
-        <div class="flex items-center justify-end mt-6">
+        <div class="mt-6">
           <button v-if="!isLoading" @click="submitData()" type="button"
-            class="px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded shadow-sm w-36 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            class="w-full px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Топшириқни яратиш
           </button>
           <button v-else type="button"
-            class="flex items-center justify-center px-3 py-2 space-x-1 text-sm font-semibold text-white bg-indigo-600 rounded shadow-sm cursor-default w-36">
+            class="flex items-center justify-center w-full px-3 py-2 space-x-1 text-sm font-semibold text-white bg-indigo-600 rounded shadow-sm cursor-default">
             <SpinnerIcon class="w-5 h-5" />
             <span>Топшириқни яратиш</span>
           </button>
