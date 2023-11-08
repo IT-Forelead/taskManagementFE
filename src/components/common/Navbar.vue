c86bca9939563bb292f87f09bd8db9adfdf30306
Surojiddin <isurojiddin@gmail.com>
2023-11-08 12:38:41 +0500

    small change

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -6,6 +6,7 @@ import ShieldCheckOutlineIcon from '../../assets/icons/ShieldCheckOutlineIcon.vu
 import AddCircleOutlineIcon from '../../assets/icons/AddCircleOutlineIcon.vue'
 import AddTaskModal from '../modals/AddTaskModal.vue'
 import EditTaskModal from '../modals/EditTaskModal.vue'
+import ViewTaskModal from '../modals/ViewTaskModal.vue'
 import LanguageDropdown from './LanguageDropdown.vue'
 import NotificationDropdown from './NotificationDropdown.vue'
 import ProfileDropdown from './ProfileDropdown.vue'
@@ -46,6 +47,7 @@ import { useModalStore } from '../../stores/modal.store'
 
   <AddTaskModal />
   <EditTaskModal />
+  <ViewTaskModal />
 </template>
 
 <style scoped></style>
