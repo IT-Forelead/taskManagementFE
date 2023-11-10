0a576976a3c49c6bbae64c43506f4a3c3a57cad4
aziz <19atabaev.aziz@gmail.com>
2023-11-10 17:12:39 +0500

    ViewTaskModal some changes

--- src/components/modals/ViewTaskModal.vue
+++ src/components/modals/ViewTaskModal.vue
@@ -1,5 +1,6 @@
 <script setup>
 import moment from "moment";
+import "moment/dist/locale/ru";
 import UserIcon from "@/assets/icons/UserIcon.vue";
 import XIcon from "@/assets/icons/XIcon.vue";
 import { useModalStore } from "../../stores/modal.store";
@@ -16,6 +17,9 @@ import ShieldCheckOutlineIcon from "../../assets/icons/ShieldCheckOutlineIcon.vu
 import UserPlusBrokenIcon from "../../assets/icons/UserPlusBrokenIcon.vue";
 import BellOutlineIcon from "../../assets/icons/BellOutlineIcon.vue";
 import PencilEditIcon from "../../assets/icons/PencilEditIcon.vue";
+import PaperclipIcon from "../../assets/icons/PaperclipIcon.vue";
+
+moment.locale("ru");
 const closeModal = () => {
   useModalStore().closeViewTaskModal();
 };
@@ -34,50 +38,64 @@ const getFullName = (userId) => {
 <template>
   <div
     v-if="useModalStore().isOpenViewTaskModal"
-    class="fixed text-xs top-0 text-gray-500 left-0 right-0 z-50 w-full max-h-screen overflow-x-hidden overflow-y-auto backdrop-blur bg-gray-900/75 md:inset-0 md:h-full"
+    class="fixed top-0 left-0 right-0 z-50 w-full max-h-screen overflow-x-hidden overflow-y-auto backdrop-blur bg-gray-900/75 md:inset-0 md:h-full"
   >
     <div
-      class="relative w-full h-full max-w-xl p-4 -translate-x-1/2 -translate-y-1/2 md:h-auto left-1/2 top-1/2"
+      class="relative w-full h-full max-w-xl p-4 -translate-x-1/2 -translate-y-1/2 md:h-auto left-1/2 top-1/2 text-gray-500"
     >
-      <div class="relative bg-white border rounded-lg shadow">
-        <div class="flex justify-between items-start p-4 rounded-t border-b">
-          <div class="flex h-full items-center space-x-2">
-            <h1>Бажариш муддати:</h1>
-            <p class="text-red-500">
-              {{ moment(selectedTask?.dueDate).format("DD/MM/YYYY") }}
-            </p>
-            <DateIcon class="text-blue-500 text-base" />
-          </div>
-          <button
-            @click="closeModal()"
-            class="text-gray-600 bg-gray-100 hover:bg-gray-200 transition-all duration-300 rounded-full p-1.5 inline-flex items-center"
+      <div class="text-xs relative bg-gray-200 flex flex-col space-y-4">
+        <div class="bg-white">
+          <header
+            class="text-sm p-4 border-b flex justify-between items-center shadow"
           >
-            <XIcon />
-          </button>
-        </div>
-        <div class="bg-white rounded-lg overflow-hidden my-4">
-          <div class="flex flex-col space-y-5 py-2 px-6">
+            <div class="flex items-center space-x-2">
+              <h1>Бажариш муддати:</h1>
+              <p class="text-red-500">
+                {{ moment(selectedTask?.dueDate).format("DD MMM YYYY") }}
+              </p>
+
+              <DateIcon class="text-blue-500 text-base" />
+            </div>
+            <button
+              @click="closeModal()"
+              class="text-gray-600 bg-gray-100 hover:bg-gray-200 transition-all duration-300 rounded-full p-1.5 inline-flex items-center"
+            >
+              <XIcon />
+            </button>
+          </header>
+          <article class="p-4 flex flex-col space-y-4">
             <div class="items-center">
-              <h1 class="font-bold">Ким томонидан яратилган</h1>
+              <h1 class="text-sm">Ким томонидан яратилган</h1>
               <p>{{ getFullName(selectedTask?.userId) }}</p>
             </div>
-            <div class="items-center">
-              <h1 class="font-bold">Топширик мазмуни</h1>
-              <p>{{ selectedTask?.description }}</p>
-              <pencil-edit-icon />
+            <div>
+              <h1 class="text-sm">Топширик мазмуни</h1>
+              <div class="flex items-center space-x-4">
+                <p>{{ selectedTask?.description }}</p>
+                <pencil-edit-icon class="text-indigo-500 text-xl" />
+              </div>
             </div>
             <div class="flex items-center space-x-1">
-              <h1 class="font-bold">Ёрлиқлар</h1>
+              <h1 class="text-sm">Ёрлиқлар</h1>
               <AddCircleOutlineIcon class="text-indigo-500 text-3xl" />
             </div>
-            <input type="file" />
-            <!--<p class="text-indigo-500"></p>Файл бириктириш<p/>-->
+            <div class="relative">
+              <input type="file" id="fileInput" class="hidden" />
+              <label
+                for="fileInput"
+                class="flex text-indigo-500 space-x-2 items-center px-2 py-2 border-2"
+              >
+                <PaperclipIcon class="flex items-start text-lg" />
+                <p>Файл бириктириш</p>
+              </label>
+            </div>
+            <!---->
             <div class="flex justify-between items-center">
               <h1 class="font-bold">Назоратчилар руйхати (1)</h1>
               <ShieldCheckOutlineIcon class="text-green-500" />
             </div>
-            <div class="flex justify-between items-center">
-              <div>
+            <div class="taskUserList flex justify-between items-center">
+              <div class="border-l px-2">
                 <h1 class="font-bold">Рахимов Улуғбек Джуманиязович</h1>
                 <h3>УП "Ургенчтрансгаз"</h3>
               </div>
@@ -97,60 +115,74 @@ const getFullName = (userId) => {
             />
             <div class="bg-green-100 p-3">
               <h1 class="font-bold">Рахимов Улуғбек Джуманиязович</h1>
-              <p>{{ moment(selectedTask?.dueDate).format("DD/MM/YYYY") }}</p>
+              <p>
+                {{ moment(selectedTask?.dueDate).format("DD MMM YYYY") }}
+              </p>
             </div>
-          </div>
+          </article>
         </div>
-        <div
-          class="bg-white flex flex-col space-y-5 rounded-lg overflow-hidden my-4"
-        >
-          <div class="flex flex-col space-y-5 py-2 px-6">
-            <div class="items-start p-4 rounded-t border-b">
-              <h1 class="font-bold">Берилган жавоблар</h1>
-            </div>
-            <div class="flex justify-between">
-              <div>
-                <button class="p-3 bg-blue-500 text-white rounded-lg">
-                  Берилган жавоб
-                </button>
-                <p>{{ moment(selectedTask?.dueDate).format("DD/MM/YYYY") }}</p>
-              </div>
-              <div>
-                <h1 class="font-bold">Рахимов Улуғбек Джуманиязович</h1>
-                <h3>УП "Ургенчтрансгаз"</h3>
-              </div>
-            </div>
-            <div class="w-full">
-              <textarea
-                class="boreder-gray-500 border-2 resize-none"
-                placeholder="Жавоб матни"
-                name=""
-                id=""
-                rows="5"
-                cols="67"
-              ></textarea>
-            </div>
-
-            <div class="flex items-center justify-between space-x-2 border-t">
-              <input type="file" />
-              <!--<p class="text-indigo-500"></p>Файл бириктириш<p/>-->
-              <button
-                class="w-36 py-2 px-4 rounded-md text-base cursor-pointer"
-              >
-                Бекор қилиш
-              </button>
-              <button>Сақлаш</button>
-            </div>
+        <div class="bg-white w-full items-star">
+          <div class="flex items-center border-b">
+            <h1 class="p-4">Берилган жавоблар</h1>
           </div>
+          <article class="p-4">
+            <div class="flex flex-col space-y-4">
+              <div class="flex justify-between">
+                <div>
+                  <button class="p-3 bg-blue-500 text-white rounded-lg">
+                    Берилган жавоб
+                  </button>
+                  <p>
+                    {{
+                      moment(selectedTask?.dueDate)
+                        .locale("ru")
+                        .format("DD MMM YYYY")
+                    }}
+                  </p>
+                </div>
+                <div>
+                  <h1 class="font-bold">Рахимов Улуғбек Джуманиязович</h1>
+                  <h3>УП "Ургенчтрансгаз"</h3>
+                </div>
+              </div>
+              <div class="taskDescriptionList">
+                <p class="border-l px-2">{{ selectedTask?.description }}</p>
+              </div>
+              <div class="w-full">
+                <textarea
+                  class="p-2 boreder-gray-500 border-2 w-full"
+                  placeholder="Жавоб матни"
+                  name=""
+                  id=""
+                  rows="5"
+                  cols="60"
+                ></textarea>
+              </div>
 
-          <div class="flex items-center justify-end p-4 space-x-2 border-t">
-            <button
-              @click="closeModal"
-              class="w-36 py-2 px-4 rounded-md text-white text-base bg-red-600 cursor-pointer hover:bg-red-800"
-            >
-              Ёпиш
-            </button>
-          </div>
+              <div class="flex items-center justify-between">
+                <div class="relative">
+                  <input type="file" id="fileInput" class="hidden" />
+                  <label
+                    for="fileInput"
+                    class="flex text-indigo-500 space-x-2 items-center px-2 py-2"
+                  >
+                    <PaperclipIcon class="flex items-start text-lg" />
+                    <p>Файл бириктириш</p>
+                  </label>
+                </div>
+                <div class="flex w-3/6 justify-between">
+                  <button class="text-base">Бекор қилиш</button>
+                  <button class="text-base px-6 py-2 rounded-full bg-gray-200">Сақлаш</button>
+                </div>
+              </div>
+              <!-- <button
+                  @click="closeModal"
+                  class="w-36 py-2 px-4 rounded-md text-white text-base bg-red-600 cursor-pointer hover:bg-red-800"
+                >
+                  Ёпиш
+                </button> -->
+            </div>
+          </article>
         </div>
       </div>
     </div>
